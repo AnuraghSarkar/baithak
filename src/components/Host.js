b95ca538bb9bb16aa993bcfb9ee9b4ae39645d0a
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2021-08-11 21:37:45 +0545

    Join Page COmpleted

--- src/components/Host.js
+++ src/components/Host.js
@@ -3,54 +3,87 @@ import joinmeet from '../img/bg.jpg'
 
 import Typical from 'react-typical'
 
-import {JoinMeetingElement, HostMeetingElement} from '../styles';
+import {
+  HeroBackground,
+  HeroImage,
+  HeroTitle,
+  TypicalTitle,
+  JoinOrHost,
+  JoinMeeting,
+  JoinTitle,
+  JoinText,
+  Or,
+  OrTitle,
+  HostMeeting,
+  HostTitle,
+  HostText,
+  JoinMeetingElement,
+  HostMeetingElement,
+  JoinButton,
+  LogButton,
+} from "../styles";
 
 
 const Hero = ()=> {
     return (
-        
-        <div className="hero-background">           
-            <img className="background-image" src = {joinmeet} alt="Kids"/> 
-            <h1 class="hero-title">Just join to  
-            </h1>
-            <h1 class="typical-title">
-            <Typical
+      <HeroBackground>
+        <HeroImage src={joinmeet} alt="ok"></HeroImage>
+        <HeroTitle>Just join to</HeroTitle>
+        <TypicalTitle>
+          <Typical
             loop={Infinity}
+            wrapper="b"
             steps={[
-                'CLOSE A DEAL',2000,
-                'DELIGHT A CUSTOMER',2000,
-                'LEAP TALL BUILDINGS ',2000,
-                'LAUNCH A PRODUCT',2000,
-                'TRAIN A TEAM',2000,
-                'SHOW WORK WHO’S BOSS',2000,
-                'DESIGN A SNEAKER',2000,
-                'MAKE A DIFFERENCE',2000,
-                'GET #*%! DONE',2000
+              "CLOSE A DEAL",
+              2000,
+              "DELIGHT A CUSTOMER",
+              2000,
+              "LEAP TALL BUILDINGS ",
+              2000,
+              "LAUNCH A PRODUCT",
+              2000,
+              "TRAIN A TEAM",
+              2000,
+              "SHOW WORK WHO’S BOSS",
+              2000,
+              "DESIGN A SNEAKER",
+              2000,
+              "MAKE A DIFFERENCE",
+              2000,
+              "GET #*%! DONE",
+              2000,
             ]}
-            />
-            </h1>
+          />
+        </TypicalTitle>
 
-            <ul class ="join-or-host">
-            <li class="host-meeting">
-                <HostMeetingElement>
-                    <h2 class="title-host">Host a Meeting</h2>
-                    <p class="host-text">Start your video conference now.</p>
-                    <button class="log-btn">Log In</button>
-                </HostMeetingElement>
-            </li>
-            <li class="or"><h2 class="or-title">OR</h2></li>
+        <JoinOrHost>
+          <HostMeeting>
+            <HostMeetingElement>
+              <HostTitle>Host a Meeting</HostTitle>
+              <HostText>
+                Want to connect to the other members? Host a meeting.
+              </HostText>
+              <LogButton>Host Meeting</LogButton>
+            </HostMeetingElement>
+          </HostMeeting>
 
-            <li class="join-meeting">
-                <JoinMeetingElement>
-                    <h2 class="title-join">Join a Meeting</h2>
-                    <p class="join-text">Did someone send you a meeting code? Please enter it here to join a meeting.</p>
-                    <button class="join-btn">Join Meeting</button>
-                </JoinMeetingElement>
-            </li>
-            
-            </ul>   
-        </div>
-    )
+          <Or>
+            <OrTitle>OR</OrTitle>
+          </Or>
+
+          <JoinMeeting>
+            <JoinMeetingElement>
+              <JoinTitle>Join a Meeting</JoinTitle>
+              <JoinText>
+                Did someone sent you a meeting code? Please enter it here to
+                join a meeting.
+              </JoinText>
+              <JoinButton>Join Meeting</JoinButton>
+            </JoinMeetingElement>
+          </JoinMeeting>
+        </JoinOrHost>
+      </HeroBackground>
+    );
 }
 
 export default Hero
--- src/styles.js
+++ src/styles.js
@@ -42,24 +42,123 @@ export const Hidden = styled.div`
   overflow: hidden;
 `;
 
-export const HeaderElement = styled(motion.div)`
-  min-height: 3.6rem;
+//hero.js page styled component
+export const HeroBackground = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  box-shadow: inset 0 0 0 1000px rgba(0, 0, 0, 0.2);
+`;
+export const HeroImage = styled(motion.img)`
   width: 100%;
+  max-height: 90vh;
+  object-fit: cover;
+`;
+export const HeroTitle = styled(motion.div)`
+  margin-top: 5rem;
+  color: #23d997;
+  position: absolute;
+  text-align: center;
+  width: 60rem;
+  text-transform: uppercase;
+  font-weight: 900;
+  font-size: 7rem;
+`;
+export const TypicalTitle = styled(motion.div)`
+  position: absolute;
+  margin-top: 15rem;
+  letter-spacing: 1rem;
+  text-align: center;
+  text-transform: uppercase;
+  font-weight: 900;
+  color: #fff;
+  text-transform: uppercase;
+  font-size: 7rem;
+`;
+export const JoinOrHost = styled(motion.div)`
+  position: absolute;
+  margin-top: 30rem;
+  list-style: none;
+  display: flex;
+  justify-content: space-around;
+  flex: 1;
+`;
+export const HostMeeting = styled(motion.div)`
+  z-index: 1;
+  text-align: center;
+  width: 30rem;
+  height: 14rem;
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(3px);
+  -webkit-backdrop-filter: blur(3px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+`;
+export const HostMeetingElement = styled(motion.div)`
+  padding-top: 2rem;
+  padding-left: 5rem;
+  width: 25rem;
+  color: white;
+  font-weight: bold;
+`;
+export const HostTitle = styled(motion.div)`
+  font-size: 1.8rem;
+  text-transform: uppercase;
+  font-weight: 900;
+`;
+export const HostText = styled(motion.div)`
+  margin-top: 0.3rem;
+`;
+
+export const LogButton = styled(motion.button)`
+  text-transform: uppercase;
+  letter-spacing: 0.01rem;
+  border: none;
+  font-size: 1.4rem;
+  color: #fff;
+  font-weight: 900;
+  margin-top: 1.4rem;
+  padding: 0.5rem;
   background-color: #ff8e00;
+  width: 18rem;
+  height: 2.8rem;
+  border-radius: 0.5rem;
 `;
-export const HeaderText = styled(motion.div)`
-  color: #ffffff;
-  font-size: 1rem;
-  padding-top: 0.9rem;
+
+export const Or = styled(motion.div)`
+  margin-left: 1rem;
+  margin-right: 1rem;
+  margin-top: 4.7rem;
+  color: #666666;
+  width: 5rem;
+  height: 5rem;
+  background: rgba(255, 255, 255, 255);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(3px);
+  -webkit-backdrop-filter: blur(3px);
+  border-radius: 10rem;
+  border: 1px solid rgba(255, 255, 255, 0.18);
 `;
-export const LernMoreButton = styled(motion.div)`
-  letter-spacing: 0.1rem;
-  padding-top: 0.5rem;
+export const OrTitle = styled(motion.div)`
+  font-size: 2.7rem;
+  font-weight: 700;
+  padding-top: 1rem;
+  padding-left: 0.35rem;
 `;
-//navbar
-export const NavbarElement = styled(motion.div)`
-  background-color: transparent;
-  margin-top: -45rem;
+
+export const JoinMeeting = styled(motion.div)`
+  z-index: 1;
+  text-align: center;
+  width: 30rem;
+  height: 14rem;
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(3px);
+  -webkit-backdrop-filter: blur(3px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
 `;
 export const JoinMeetingElement = styled(motion.div)`
   padding-top: 2rem;
@@ -68,11 +167,26 @@ export const JoinMeetingElement = styled(motion.div)`
   color: #fff;
   font-weight: bold;
 `;
+export const JoinTitle = styled(motion.div)`
+  font-size: 1.8rem;
+  text-transform: uppercase;
+  font-weight: 900;
+`;
+export const JoinText = styled(motion.div)`
+  margin-top: 0.3rem;
+`;
 
-export const HostMeetingElement = styled(motion.div)`
-  padding-top: 2rem;
-  padding-left: 5rem;
-  width: 25rem;
-  color: white;
-  font-weight: bold;
+export const JoinButton = styled(motion.button)`
+  text-transform: uppercase;
+  letter-spacing: 0.01rem;
+  border: none;
+  font-size: 1.4rem;
+  color: #fff;
+  font-weight: 900;
+  margin-top: 1.4rem;
+  padding: 0.5rem;
+  background-color: #24b4d8;
+  width: 18rem;
+  height: 2.8rem;
+  border-radius: 0.5rem;
 `;
